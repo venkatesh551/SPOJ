130e3fea1453a26129c57f4c107afa6b5d8f4d76
Venkateswarlu Pallamala <p.venkatesh551@gmail.com>
2015-05-05 09:41:46 +0530

    To-Do-List problems

---
+++ 2015/May/MODIFY_SEQUENCE.py
@@ -0,0 +1,8 @@
+def main():
+    t = int(raw_input())
+    for _ in xrange(t):
+        n = int(raw_input())
+        A = map(int, raw_input().split())
+
+if __name__ == '__main__':
+    main()
---
+++ 2015/May/Manku_Word.py
@@ -0,0 +1,9 @@
+def main():
+    t = int(raw_input())
+    for _ in xrange(t):
+        n = int(raw_input())
+        print 'm'
+
+
+if __name__ == '__main__':
+    main()
