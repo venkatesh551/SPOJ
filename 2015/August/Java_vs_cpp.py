4bac1730f3779dae7df228063dab25e710acbbe9
Venkateswarlu Pallamala <p.venkatesh551@gmail.com>
2015-08-18 10:44:01 +0530

    solved JAVA_vs_C++

--- 2015/August/Java_vs_cpp.py
+++ 2015/August/Java_vs_cpp.py
@@ -8,19 +8,41 @@ import sys
 
 
 def translate_to_cpp(inp):
-    
+    result = ''
+    for ch in inp:
+        if ch.isupper():
+            result += '_' + ch.lower()
+        else:
+            result += ch
+    return result
+
+
+def translate_to_java(inp):
+    result = ''
+    prev = 'x'
+    for ch in inp:
+        if ch.isupper():
+            return "Error!"
+        elif prev == '_':
+            result += ch.upper()
+        elif ch != "_":
+            result += ch
+        prev = ch
+    return result
 
 
 def translate(inp):
+    if not inp[0].islower() or inp.endswith('_') or inp.find('__') >= 0:
+        return "Error!"
     if inp.find('_') == -1:
-        translate_to_cpp(inp)
+        return translate_to_cpp(inp)
     else:
-        translate_to_java(inp)
+        return translate_to_java(inp)
 
 
 def main():
     while True:
-        inp = sys.stdin.readline()
+        inp = sys.stdin.readline().rstrip()
         if not inp:
             return
         print translate(inp)
